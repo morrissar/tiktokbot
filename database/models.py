37562e0d20076de750279c9558ccdb9398e80901
morrissar <morisarchik@gmail.com>
2025-11-25 17:19:15 +0800

    Update models.py

--- database/models.py
+++ database/models.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 import os
 
-engine = create_async_engine(url='sqlite+aiosqlite:////tmp/db.sqlite3')
+# Исправьте путь к базе данных
+engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3')
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
@@ -21,12 +22,4 @@ class User(Base):
 async def async_main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-
-
-
-
-
-
-
-
+    print("✅ Таблицы созданы успешно")
