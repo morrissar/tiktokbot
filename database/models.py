7ce7cb48e801b746ddd06ce71ab1a43057ae43b1
morrissar <morisarchik@gmail.com>
2025-11-28 15:24:40 +0800

    Update models.py

--- database/models.py
+++ database/models.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 import os
 
-# ИСПРАВЛЕННЫЙ ПУТЬ - сохраняем в постоянной директории
 engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3')
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
